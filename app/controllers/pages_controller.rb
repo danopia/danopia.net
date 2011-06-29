dc9967405c732edc01d9300e3a4abecb89e13374
Daniel Lamando <me@danopia.net>
2011-06-29 00:11:10 -0400

    Cache pages up to 1 hour

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,14 @@
 class PagesController < ApplicationController
-  def statement
-  end
+  before_filter :cache
+  
+  def statement; end
+  def projects; end
+  def technical; end
+  def resume; end
 
-  def projects
-  end
-
-  def technical
-  end
-
-  def resume
-  end
-
+  private
+    def cache
+      response.headers['Cache-Control'] = 'public, max-age=3600'
+    end
 end
+
